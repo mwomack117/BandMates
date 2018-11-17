f279748692410ec195a7c34b980acabf96f29151
mwomack <mikewomack2000@yahoo.com>
2018-11-17 12:55:17 -0800

    seeds file and updated model

---
+++ models/seeds.sql
@@ -0,0 +1,11 @@
+INSERT into Musicians ( name, soloOrBand, instrument, genre, yearsExp, bio) 
+VALUES ("mike", true, "guitar", "metal, rock, classical, jazz", 10, "I most enjoy playing jazz and funk improvisation, as well as classical. Love to go to live show!");
+
+INSERT into Musicians ( name, soloOrBand, instrument, genre, yearsExp, bio) 
+VALUES ("zach", true, "drums", "rock", 15, "I also like to play bass guitar");
+
+INSERT into Musicians ( name, soloOrBand, instrument, genre, yearsExp, bio) 
+VALUES ("Eminem", false, "vocalist", "rap/hip hop", 20, "I like to drop ill raps over dope beats.");
+
+INSERT into Musicians ( name, soloOrBand, instrument, genre, yearsExp, bio) 
+VALUES ("Amber", false, "vocalist", "pop/alternative", 7, "My favorite singers are Madonna and Beyonce.");
