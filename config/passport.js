1786bf5903d265e5fb23c04b4aff184395b8be18
Amber <amberrose9167@gmail.com>
2018-11-26 15:02:21 -0800

    finished adding all the passport code

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,4 @@
+//load bcrypt
 var bCrypt = require('bcrypt-nodejs');
 
 module.exports = function (passport, musician) {
@@ -15,7 +16,7 @@ module.exports = function (passport, musician) {
             };
             Musician.findOne({
                 where: {
-                    username: username
+                    username: userName
                 }
             }).then(function (musician) {
 
@@ -55,6 +56,95 @@ module.exports = function (passport, musician) {
                     });
                 }
             });
+        },
+    ));
+    //serialize
+    passport.serializeUser(function (musician, done) {
+
+        done(null, user.id);
+
+    });
+    // deserialize user 
+    passport.deserializeUser(function (id, done) {
+
+        Musician.findById(id).then(function (musician) {
+
+            if (musician) {
+
+                done(null, musician.get());
+
+            } else {
+
+                done(musician.errors, null);
+
+            }
+
+        });
+
+    });
+    //LOCAL SIGNIN
+    passport.use('local-signin', new LocalStrategy(
+
+        {
+
+            // by default, local strategy uses username and password, we will override with email
+
+            usernameField: 'userName',
+
+            passwordField: 'password',
+
+            passReqToCallback: true // allows us to pass back the entire request to the callback
+
+        },
+
+
+        function (req, userName, password, done) {
+
+            var isValidPassword = function (userpass, password) {
+
+                return bCrypt.compareSync(password, userpass);
+
+            }
+
+            Musician.findOne({
+                where: {
+                    username: userName
+                }
+            }).then(function (musician) {
+
+                if (!musician) {
+
+                    return done(null, false, {
+                        message: 'That User name does not exist'
+                    });
+
+                }
+
+                if (!isValidPassword(musician.password, password)) {
+
+                    return done(null, false, {
+                        message: 'Incorrect password.'
+                    });
+
+                }
+
+
+                var userinfo = musician.get();
+                return done(null, userinfo);
+
+
+            }).catch(function (err) {
+
+                console.log("Error:", err);
+
+                return done(null, false, {
+                    message: 'Something went wrong with your Signin'
+                });
+
+            });
+
+
         }
+
     ));
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,53 @@
 var exports = module.exports = {}
- 
-module.exports = function(app,passport) {
-    
-    app.get('/signup', function(req, res) {
- 
-        res.render('signup');
-        
-    } );
 
-    app.get('/landing', function(req, res) {
- 
+module.exports = function (app, passport) {
+    //get routes
+    app.get('/index', function (req, res) {
+
+        res.render('index');
+
+    });
+
+    app.get('/landing', function (req, res) {
+
         res.render('landing');
-     
-    } );
 
-    app.post('/signup', passport.authenticate('local-signup', {
+    });
+
+    app.get('/dashboard', isLoggedIn, function (req, res) {
+
+        res.render('dashboard');
+    });
+
+    app.get('/logout', function (req, res) {
+
+        req.session.destroy(function (err) {
+
+            res.redirect('/');
+
+        });
+    });
+    function isLoggedIn(req, res, next) {
+
+        if (req.isAuthenticated())
+
+            return next();
+
+        res.redirect('/landing');
+
+    }
+    //post routes
+    app.post('/index', passport.authenticate('local-signup', {
+        successRedirect: '/dashboard',
+
+        failureRedirect: '/index'
+    }));
+
+    app.post('/landing', passport.authenticate('local-signin', {
         successRedirect: '/dashboard',
  
-        failureRedirect: '/signup'
+        failureRedirect: '/landing'
     }
  
 ));
- 
 }
